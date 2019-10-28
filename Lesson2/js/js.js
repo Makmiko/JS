7d61ada275803389cdf610f601fde5abad81a2d1
Makar <kondratiev.makar@yandex.ru>
2019-10-28 19:36:18 +0300

    Done

--- Lesson2/js/js.js
+++ Lesson2/js/js.js
@@ -9,7 +9,7 @@ function findSumSeven(arr) {
     }
   }
 }
-findSumSeven([3, 5, 7, 0]);
+findSumSeven([3, 5, 10, 2, 5]);
 
 //Вторая задача
 function threeFive(min, max) {
@@ -31,3 +31,46 @@ function threeFive(min, max) {
   return 0;
 }
 threeFive(1, 100);
+
+//Третья задача
+function washPlates(plate_count, detergent) {
+  for (let i = 0; i <= plate_count; ) {
+    console.log(
+      `${plate_count} plates to wash and ${detergent} detergent to use`
+    );
+    if (detergent > 0 && plate_count != 0) {
+      plate_count--;
+      detergent -= 0.5;
+      console.log(`${plate_count} plates and ${detergent} detergent`);
+    } else if (detergent <= 0 && plate_count > 0) {
+      return `Detergen ended, you have still ${plate_count} plates to wash`;
+    } else return "All plates washed";
+  }
+}
+console.log(washPlates(20, 20));
+
+//Четвертая задача
+function averageSum(arr1, arr2) {
+  if (arr1.length != arr2.length) return "Arrays are not equal length";
+  console.log(arr1, arr2);
+  let aver1 = 0;
+  for (item of arr1) aver1 += item;
+  aver1 /= arr1.length;
+  let aver2 = 0;
+  for (item of arr2) aver2 += item;
+  aver2 /= arr2.length;
+  if (aver1 > aver2) {
+    return "First array's average sum is bigger";
+  } else if (aver1 == aver2) {
+    return "Arrays are equal";
+  } else return "Second array's average sum is bigger";
+}
+console.log([1, 2, 3], [1, 2]); //Проверка на массивы разной длины
+console.log(
+  averageSum([2, 3, 4, 5, 63, 623, 3], [25, 63, 2412, 13, 52513, 5, 1]) //Проверка на правильное решение задачи
+);
+
+for (let i = 0; i < 10; i++) {
+  console.log("#" + " ".repeat(i) + "#");
+  i == 9 ? console.log("# ".repeat(i - 2)) : 0;
+}
